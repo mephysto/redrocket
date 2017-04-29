29168ce361dba815c1a391b3374963ef3b0ad1c8
benderham <ben@benderham.com>
2017-04-29 17:44:20 +0800

    match page

--- src/app/MatchDetail.js
+++ src/app/MatchDetail.js
@@ -8,8 +8,33 @@ class MatchDetail extends Component {
   }
   render() {
     return (
-      <div className="container">
-        MatchDetail
+      <div className="container match__container">
+        <h1 className="match__heading">It's a Match!</h1>
+        <div className="matches">
+          <div className="profile">
+            <div className="profile__image"></div>
+            <div className="profile__details">
+              <div className="profile__name">Emma</div>
+              <div className="profile__rating">4.45</div>
+            </div>
+          </div>
+          <div className="profile">
+            <div className="profile__image"></div>
+            <div className="profile__details">
+              <div className="profile__name">Emma</div>
+              <div className="profile__rating">4.45</div>
+            </div>
+          </div>
+        </div>
+        <div className="match__button">Get Started</div>
+        <div className="match__menu">
+          What's on the menu:<br />
+          <span>Harissa Vegetable<br />and Egg Tray Bake</span>
+        </div>
+        <div className="match__time">
+          <span>@7PM</span><br />
+          add to calendar &raquo;
+        </div>
       </div>
     );
   }
