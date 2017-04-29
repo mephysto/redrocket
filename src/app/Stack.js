ff4066ecef1ccc5b1394b7dae1625dcc0a6ce9fc
benderham <ben@benderham.com>
2017-04-29 14:41:46 +0800

    stack change

--- src/app/Stack.js
+++ src/app/Stack.js
@@ -48,8 +48,8 @@ class Stack extends Component {
                 <div className="card__image"></div>
                 <div className="card__details">
                   <h1 className="card__heading">Harissa Vegetable and egg tray bake</h1>
-                  <div className="card__detail">Est. eat time 7pm</div>
-                  <div className="card__detail">$13</div>
+                  <div className="card__detail card__detail--clock">Est. eat time 7pm</div>
+                  <div className="card__detail card__detail--user">$13</div>
                 </div>
                 <div className="profile">
                   <div className="profile__image"></div>
