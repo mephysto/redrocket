c4fbdc6a7be0f38cb98a59b59bca784edc505adb
Ben Derham <bderham@marketforce.com.au>
2017-04-29 13:03:36 +0800

    card

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './Header';
-import Footer from './Footer';
-
-
 import Landing from './Landing';
 import MatchDetail from './MatchDetail';
 import Messages from './Messages';
 import Detail from './Detail';
+import Things from './Things';
 
 // import logo from '../logo.svg';
 import '../styles/global.min.css';
@@ -18,12 +15,11 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Header />
           <Route exact path="/" component={Landing} />
           <Route path="/matchdetail" component={MatchDetail} />
           <Route path="/detail" component={Detail} />
           <Route path="/messages" component={Messages} />
-          <Footer /> 
+          <Route path="/things" component={Things} />
         </div>       
       </Router>
     );
